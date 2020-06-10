9e03436f1c47c7418bfc50d983178dbf3d290e72
Joshua Cold <joshua.cold@microfocus.com>
2020-06-10 08:35:05 -0600

    actually add dwm

---
+++ .dwm/autostart.sh
@@ -0,0 +1,11 @@
+autorandr &
+xfsettingsd &
+hsetroot -solid "#0B5C70" &
+setxkbmap -option caps:escape &
+nm-applet  &
+pamac-tray &
+wmnme LG3D &
+keynav &
+kdeconnect-indicator &
+blueman-applet &
+
