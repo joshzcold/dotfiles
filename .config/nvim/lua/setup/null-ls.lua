f3d5c6b73b5cd344de6c4b702dd97eda8a40e621
Joshua Cold <joshua.cold@securitymetrics.com>
2022-01-04 14:29:05 -0700

    turn back on auto linting

--- .config/nvim/lua/setup/null-ls.lua
+++ .config/nvim/lua/setup/null-ls.lua
@@ -29,5 +29,5 @@ require("null-ls").setup({
   sources = lSsources,
 })
 -- the duration in there is to stop timeouts on massive files
--- vim.cmd("autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync(nil, 7500)")
+vim.cmd("autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync(nil, 7500)")
 vim.o.updatetime = 250
