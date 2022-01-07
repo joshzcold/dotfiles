0ceca549dc45909cccd3dc00f0a7f20dfc727ea1
Joshua Cold <joshua.cold@securitymetrics.com>
2022-01-07 14:24:42 -0700

    update autostart and fix git push script

--- .config/usr-scripts/git_push.sh
+++ .config/usr-scripts/git_push.sh
@@ -3,7 +3,7 @@
 exec 5>&1
 push_out=$(git push -u 2>&1 | tee >(cat - >&5))
 
-if echo "$push_out" | grep "pull-requests"; then
+if echo "$push_out" | grep "Create pull-requests"; then
 	url=$(echo "$push_out" | grep -oP "https://.*")
 	qutebrowser ":open -t $url"
 fi
--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,17 +2,13 @@ nitrogen --restore &
 autorandr -c &
 xfsettingsd &
 # hsetroot -solid "#0B5C70" &
-emacs --daemon &
+# emacs --daemon &
 nm-applet &
 dunst &
 pamac-tray &
-wmnme LG3D &
 picom &
-kalarm &
-keynav &
+# keynav &
 blueman-applet &
 volctl &
-sleep 5
 /home/joshua/.config/usr-scripts/check-on-dotfiles &
 kdeconnect-indicator &
-
