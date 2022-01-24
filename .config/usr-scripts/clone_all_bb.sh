b9b12b056ca2adc5ccc5051b41fb40659a8a3f1d
Joshua Cold <joshua.cold@securitymetrics.com>
2022-01-24 13:34:11 -0700

    add new user scripts

---
+++ .config/usr-scripts/clone_all_bb.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+server="${BITBUCKETSERVER:-"https://bitbucket.secmet.co"}"
+clone_type="${CLONE_TYPE:-"ssh"}"
+
+PROJECTS="$(curl -s -H "Authorization: Bearer $TOKEN" --request GET "$server/rest/api/1.0/projects?limit=999" | jq --raw-output ".values[].key")"
+
+pushd ~/git
+
+while IFS= read -r PROJECT; do
+	PROJECT=$(echo "$PROJECT" | awk '{print tolower($0)}')
+	repos=$(curl -s -H "Authorization: Bearer $TOKEN" --request GET "$server/rest/api/1.0/projects/$PROJECT/repos?limit=999" | jq --raw-output ".values[].links.clone[] | select(.name == \"$clone_type\") | .href")
+	mkdir -p "${PROJECT}"
+	cd "${PROJECT}" || exit
+	echo "$repos" | parallel -I% git clone %
+	cd ../
+done <<<"$PROJECTS"
+
+popd
