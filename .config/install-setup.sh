a1e140d35ef46437e048a76445daff33a564e25b
Joshua Cold <joshua.cold@securitymetrics.com>
2022-08-29 12:06:10 -0600

    update dotfiles from joshua-home

--- .config/install-setup.sh
+++ .config/install-setup.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
-set -x
 
 function user_prompt(){
   read -p "$1 [y/n]" -n 1 -r
   echo    # (optional) move to a new line
 }
 
-user_prompt "Install git"
+user_prompt "Install git, base-devel, yadm"
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   sudo pacman -S --needed git base-devel yadm
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "use yadm and get dotfiles"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   yadm clone https://github.com/joshzcold/dotfiles.git
   yadm checkout .
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Setup dwm"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   cd ~/.dwm
   sudo make install
   sudo cp run-dwm /usr/bin/run-dwm
-  sudo cat <<EOT >> /usr/share/xsessions/dwm.desktop
+  sudo tee /usr/share/xsessions/dwm.desktop > /dev/null <<EOT
 [Desktop Entry]
 Version=1.0
 Name=dwm
@@ -34,64 +38,81 @@ Exec=run-dwm
 Icon=
 Type=Application
 EOT
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Install yay"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   cd /tmp
   git clone https://aur.archlinux.org/yay-bin.git
   cd yay-bin
   makepkg -si
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Install all applications"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   cd ~
   yay -S --needed - < ~/.config/install-apps
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Setup dictionary qutebrowser"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   /usr/share/qutebrowser/scripts/dictcli.py install en-US
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Setup ssh keys go to github.com and copy ssh keys to ~/.ssh"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  read "MANUAL STEP"
+  set -x
+  read -p "MANUAL STEP"
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Setup kmonad"
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   sudo groupadd uinput
   sudo usermod -aG input $USER
   sudo usermod -aG uinput $USER
-  sudo echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' > /etc/udev/rules.d/90-kmonad.rules
+  echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/90-kmonad.rules
   sudo modprobe uinput
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "setup perimeter81"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   yay --save --editmenu -S perimeter81
   user_prompt "Add --no-sandbox to perimeter81.desktop Exec"
-  read "MANUAL STEP"
+  read -p "MANUAL STEP"
   sudo nvim /usr/share/applications/perimeter81.desktop
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Install neovim plugins"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   nvim -c PackerSync
+  { set +x; } 2> /dev/null 
 fi
 
 user_prompt "Change yadm remote to ssh"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  set -x
   yadm remote set-url origin git@github.com:joshzcold/dotfiles.git
+  { set +x; } 2> /dev/null 
 fi
