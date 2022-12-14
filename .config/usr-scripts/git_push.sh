913cfe86bd775b7b481d3c8ea99ad265e752d74c
Joshua Cold <joshua.cold@securitymetrics.com>
2022-12-14 10:52:52 -0700

    update dotfiles from joshua-home

--- .config/usr-scripts/git_push.sh
+++ .config/usr-scripts/git_push.sh
@@ -7,12 +7,3 @@ if echo "$push_out" | grep "Create pull request"; then
 	url=$(echo "$push_out" | grep -oP "https://.*")
 	qutebrowser ":open -t $url"
 fi
-
-git_repo="$(basename $(git rev-parse --show-toplevel))"
-if [[ ${git_repo} =~ .*\.role ]]; then
-	cd ~/git/in/master.inventory
-	set -x
-	ur $(echo $git_repo | sed 's/.role//g')
-	{ set +x; } 2>/dev/null
-	cd -
-fi
