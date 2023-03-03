0500d65a97e621d59c6f9d0308e6115771362a9b
Joshua Cold <joshua.cold@securitymetrics.com>
2023-03-02 17:10:20 -0700

    update dotfiles from joshua-home

--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -3,8 +3,8 @@ return {
         "L3MON4D3/LuaSnip",
         version = "v1.2.*",
         config = function()
-            -- local ls = require("luasnip")
-            -- ls.filetype_extend("all", { "_" })
+            local ls = require("luasnip")
+            ls.filetype_extend("all", { "_" })
             require("luasnip.loaders.from_snipmate").lazy_load()
         end,
         dependencies = {
