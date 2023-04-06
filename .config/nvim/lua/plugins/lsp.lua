42455b3f4b842ad52d12a0b998389b8531e2ce01
Joshua Cold <joshua.cold@securitymetrics.com>
2023-04-06 16:03:41 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -74,7 +74,7 @@ return {
         ensure_installed = {
           "lua_ls",
           "bashls",
-          "groovy-language-server",
+          "groovyls",
           "pyright",
           "ansiblels",
           "yamlls",
