3b58da9a91fa55acbf1b8aaa0821240e0ee42c51
Joshua Cold <joshua.cold@securitymetrics.com>
2023-04-27 11:40:48 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -1,6 +1,6 @@
 local function git_write_with_input()
       vim.cmd.Gwrite()
-      vim.api.nvim_command(":!git add % ")
+      vim.api.nvim_command(":!git add $(readlink -f %)")
       local message = vim.fn.input("Commit message: ")
       vim.api.nvim_command(":!git commit -m " .. "'" .. message .. "'")
       return message
