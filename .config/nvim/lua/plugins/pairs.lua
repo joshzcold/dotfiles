c553174dbc57b387f682e717707e73df428a0290
Joshua Cold <joshua.cold@securitymetrics.com>
2023-06-02 11:51:58 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/pairs.lua
+++ .config/nvim/lua/plugins/pairs.lua
@@ -1,27 +1,28 @@
 return {
-    {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end,
-    },
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup({
-                ignored_next_char = "[%w%.%(%[%{%<%\"%']",
-                fast_wrap = {
-                    map = "<C-p>",
-                    chars = { "{", "[", "(", '"', "'", "`" },
-                    pattern = string.gsub([[ [%'%"%`%)%>%]%)%}%,] ]], "%s+", ""),
-                    offset = 0, -- Offset from pattern match
-                    end_key = "$",
-                    keys = "qwertyuiopzxcvbnmasdfghjkl",
-                    check_comma = true,
-                    highlight = "Search",
-                    highlight_grey = "Comment",
-                },
-            })
-        end,
-    },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({
+        ignored_next_char = "%S",
+        disable_in_macro = true,
+        fast_wrap = {
+          map = "<C-p>",
+          chars = { "{", "[", "(", '"', "'", "`" },
+          pattern = string.gsub([[ [%'%"%`%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0, -- Offset from pattern match
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "Search",
+          highlight_grey = "Comment",
+        },
+      })
+    end,
+  },
 }
