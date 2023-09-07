f3e5d72fb54010f29ee40932a368a69145fee45b
Joshua Cold <joshua.cold@securitymetrics.com>
2023-09-07 16:17:11 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/display.lua
+++ .config/nvim/lua/plugins/display.lua
@@ -24,48 +24,48 @@ return {
 	{
 		"folke/noice.nvim",
 		config = function()
-			require("noice").setup({
-				-- add any options here
-				cmdline = {
-					view = "cmdline",
-				},
-				lsp = {
-					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
-					routes = {
-						{
-							view = "cmdline",
-							filter = {
-								any = {
-									{ event = "msg_show", kind = "confirm_sub" },
-								},
-							},
-						},
-						{
-							view = "notify",
-							filter = { event = "msg_showmode" },
-						},
-					},
-				},
-				messages = {
-					enabled = true,
-				},
-				notify = {
-					enabled = true,
-				},
-				-- you can enable a preset for easier configuration
-				presets = {
-					bottom_search = true, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = false, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = true, -- add a border to hover docs and signature help
-				},
-			})
+			-- require("noice").setup({
+			-- 	-- add any options here
+			-- 	cmdline = {
+			-- 		view = "cmdline",
+			-- 	},
+			-- 	lsp = {
+			-- 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			-- 		override = {
+			-- 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			-- 			["vim.lsp.util.stylize_markdown"] = true,
+			-- 			["cmp.entry.get_documentation"] = true,
+			-- 		},
+			-- 		routes = {
+			-- 			{
+			-- 				view = "cmdline",
+			-- 				filter = {
+			-- 					any = {
+			-- 						{ event = "msg_show", kind = "confirm_sub" },
+			-- 					},
+			-- 				},
+			-- 			},
+			-- 			{
+			-- 				view = "notify",
+			-- 				filter = { event = "msg_showmode" },
+			-- 			},
+			-- 		},
+			-- 	},
+			-- 	messages = {
+			-- 		enabled = true,
+			-- 	},
+			-- 	notify = {
+			-- 		enabled = true,
+			-- 	},
+			-- 	-- you can enable a preset for easier configuration
+			-- 	presets = {
+			-- 		bottom_search = true, -- use a classic bottom cmdline for search
+			-- 		command_palette = true, -- position the cmdline and popupmenu together
+			-- 		long_message_to_split = false, -- long messages will be sent to a split
+			-- 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			-- 		lsp_doc_border = true, -- add a border to hover docs and signature help
+			-- 	},
+			-- })
 		end,
 		dependencies = {
 			{
