dfec9ebfaff5f72bf0b892585cc5d9de41515051
Joshua Cold <joshua.cold@securitymetrics.com>
2023-12-05 14:57:03 -0700

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,224 +1,206 @@
 return {
 
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = true },
-		},
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "folke/neodev.nvim" },
-		},
-		---@class PluginLspOpts
-		config = function()
-			-- LSP settings
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      inlay_hints = { enabled = true },
+    },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'folke/neodev.nvim' },
+    },
+    ---@class PluginLspOpts
+    config = function()
+      -- LSP settings
 
-			local border = {
-				{ "┌", "FloatBorder" },
-				{ "─", "FloatBorder" },
-				{ "┐", "FloatBorder" },
-				{ "│", "FloatBorder" },
-				{ "┘", "FloatBorder" },
-				{ "─", "FloatBorder" },
-				{ "└", "FloatBorder" },
-				{ "│", "FloatBorder" },
-			}
-			-- Add the border on hover and on signature help popup window
-			local handlers = {
-				["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-				["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-			}
-			-- Add border to the diagnostic popup window
-			vim.diagnostic.config({
-				virtual_text = {
-					prefix = "■ ", -- Could be '●', '▎', 'x', '■', , 
-				},
-				float = { border = border },
-			})
-			-- make nvim-cmp aware of extra capabilities coming from lsp
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-			capabilities.textDocument.foldingRange = {
-				dynamicRegistration = false,
-				lineFoldingOnly = true,
-			}
+      local border = {
+        { '┌', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '┐', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+        { '┘', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '└', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+      }
+      -- Add the border on hover and on signature help popup window
+      local handlers = {
+        ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+        ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+      }
+      -- Add border to the diagnostic popup window
+      vim.diagnostic.config {
+        virtual_text = {
+          prefix = '■ ', -- Could be '●', '▎', 'x', '■', , 
+        },
+        float = { border = border },
+      }
+      -- make nvim-cmp aware of extra capabilities coming from lsp
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
 
-			vim.g.diagnostics_active = true
-			function _G.toggle_diagnostics()
-				if vim.g.diagnostics_active then
-					vim.g.diagnostics_active = false
-					vim.diagnostic.disable()
-				else
-					vim.g.diagnostics_active = true
-					vim.diagnostic.enable()
-				end
-			end
+      -- disable virtal text for corn.nvim
+      vim.diagnostic.config { virtual_text = false }
+      vim.g.diagnostics_active = true
+      function _G.toggle_diagnostics()
+        if vim.g.diagnostics_active then
+          vim.g.diagnostics_active = false
+          vim.diagnostic.disable()
+        else
+          vim.g.diagnostics_active = true
+          vim.diagnostic.enable()
+        end
+      end
 
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>lt",
-				":call v:lua.toggle_diagnostics()<CR>",
-				{ desc = "toggle_diagnostics" }
-			)
-			-- Use an on_attach function to only map the following keys
-			-- after the language server attaches to the current buffer
-			local on_attach = function(client, bufnr)
-				vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-				-- Mappings.
-				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", { desc = "lsp declaration" })
-				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "lsp definition" })
-				vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "lsp code action" })
-				-- don't set the keywordprg if we have one we already want
-				if vim.opt_local.keywordprg:get() == "" then
-					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "lsp buffer hover" })
-				end
-				vim.keymap.set(
-					"n",
-					"<leader>i",
-					"<cmd>lua vim.lsp.buf.implementation()<cr>",
-					{ desc = "lsp implementation" }
-				)
-				vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", { desc = "lsp type defintion" })
-				vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "lsp rename" })
-				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", { desc = "lsp references" })
-				vim.keymap.set(
-					"n",
-					"<leader>e",
-					"<cmd>lua vim.diagnostic.open_float()<cr>",
-					{ desc = "lsp diagnostics" }
-				)
-				vim.keymap.set(
-					"n",
-					"[d",
-					"<cmd>lua vim.diagnostic.goto_prev()<cr>",
-					{ desc = "lsp diagnostic goto next" }
-				)
-				vim.keymap.set(
-					"n",
-					"]d",
-					"<cmd>lua vim.diagnostic.goto_next()<cr>",
-					{ desc = "lsp diagnostic goto prev" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>q",
-					"<cmd>lua vim.diagnostic.set_loclist()<cr>",
-					{ desc = "lsp diagnostic qixfix" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>ld",
-					"<cmd>lua vim.diagnostic.disable()<cr>",
-					{ desc = "lsp diagnostic disable" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>le",
-					"<cmd>lua vim.diagnostic.enable()<cr>",
-					{ desc = "lsp diagnostic enable" }
-				)
-			end
+      vim.api.nvim_set_keymap('n', '<leader>lt', ':call v:lua.toggle_diagnostics()<CR>', { desc = 'toggle_diagnostics' })
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        -- Mappings.
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { desc = 'lsp declaration' })
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = 'lsp definition' })
+        vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', { desc = 'lsp code action' })
+        -- don't set the keywordprg if we have one we already want
+        if vim.opt_local.keywordprg:get() == '' then
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', { desc = 'lsp buffer hover' })
+        end
+        vim.keymap.set('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<cr>', { desc = 'lsp implementation' })
+        vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { desc = 'lsp type defintion' })
+        vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', { desc = 'lsp rename' })
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', { desc = 'lsp references' })
+        vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>', { desc = 'lsp diagnostics' })
+        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', { desc = 'lsp diagnostic goto next' })
+        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', { desc = 'lsp diagnostic goto prev' })
+        vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.set_loclist()<cr>', { desc = 'lsp diagnostic qixfix' })
+        vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.diagnostic.disable()<cr>', { desc = 'lsp diagnostic disable' })
+        vim.keymap.set('n', '<leader>le', '<cmd>lua vim.diagnostic.enable()<cr>', { desc = 'lsp diagnostic enable' })
+      end
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require 'lspconfig'
 
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"bashls",
-					"groovyls",
-					"pylsp",
-					"ansiblels",
-					"yamlls",
-					"jsonls",
-					"tailwindcss",
-					"tsserver",
-				},
-				automatic_installation = true,
-			})
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          'lua_ls',
+          'bashls',
+          'groovyls',
+          'pylsp',
+          'ansiblels',
+          'yamlls',
+          'jsonls',
+          'tailwindcss',
+          'tsserver',
+        },
+        automatic_installation = true,
+      }
 
-			require("neodev").setup({})
+      require('neodev').setup {}
 
-			require("mason-lspconfig").setup_handlers({
-				-- The first entry (without a key) will be the default handler
-				-- and will be called for each installed server that doesn't have
-				-- a dedicated handler.
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-						handlers = handlers
-					})
-				end,
-				-- Next, you can provide a dedicated handler for specific servers.
-				["ansiblels"] = function()
-					lspconfig.ansiblels.setup({
-						settings = {
-							ansible = {
-								ansible = {
-									useFullyQualifiedCollectionNames = true,
-								},
-								validation = {
-									lint = {
-										enabled = true,
-										arguments = "-x role-name,package-latest,fqcn-builtins",
-									},
-								},
-							},
-						},
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["yamlls"] = function()
-					lspconfig.yamlls.setup({
-						settings = {
-							yaml = {
-								keyOrdering = false,
-							},
-						},
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["lua_ls"] = function()
-					lspconfig.lua_ls.setup({
-						settings = {
-							Lua = {
-								hint = { enable = true },
-								workspace = {
-									checkThirdParty = false,
-								},
-							},
-						},
-					})
-				end,
-			})
-		end,
-	},
-	"onsails/lspkind-nvim",
-	"nvim-lua/lsp-status.nvim",
-	{ "folke/trouble.nvim" },
-	{
-		"danymat/neogen",
-		config = function()
-			require("neogen").setup({
-				enabled = true,
-			})
-		end,
-	},
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		config = function()
-			require("fidget").setup({
-				window = { winblend = 0 },
-				align = {
-					bottom = false,
-				},
-			})
-		end,
-	},
+      require('mason-lspconfig').setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function(server_name) -- default handler (optional)
+          require('lspconfig')[server_name].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            handlers = handlers,
+          }
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        ['ansiblels'] = function()
+          lspconfig.ansiblels.setup {
+            settings = {
+              ansible = {
+                ansible = {
+                  useFullyQualifiedCollectionNames = true,
+                },
+                validation = {
+                  lint = {
+                    enabled = true,
+                    arguments = '-x role-name,package-latest,fqcn-builtins',
+                  },
+                },
+              },
+            },
+            on_attach = on_attach,
+            capabilities = capabilities,
+          }
+        end,
+        ['yamlls'] = function()
+          lspconfig.yamlls.setup {
+            settings = {
+              yaml = {
+                keyOrdering = false,
+              },
+            },
+            on_attach = on_attach,
+            capabilities = capabilities,
+          }
+        end,
+        ['lua_ls'] = function()
+          lspconfig.lua_ls.setup {
+            settings = {
+              Lua = {
+                hint = { enable = true },
+                workspace = {
+                  checkThirdParty = false,
+                },
+              },
+            },
+          }
+        end,
+      }
+    end,
+  },
+  'onsails/lspkind-nvim',
+  'nvim-lua/lsp-status.nvim',
+  { 'folke/trouble.nvim' },
+  {
+    'danymat/neogen',
+    config = function()
+      require('neogen').setup {
+        enabled = true,
+      }
+    end,
+  },
+  {
+    'j-hui/fidget.nvim',
+    tag = 'legacy',
+    config = function()
+      require('fidget').setup {
+        window = { winblend = 0 },
+        align = {
+          bottom = false,
+        },
+      }
+    end,
+  },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
+  {
+    'RaafatTurki/corn.nvim',
+    event = 'VeryLazy',
+    opts = {
+      on_toggle = function(is_hidden)
+        vim.diagnostic.config { virtual_text = not vim.diagnostic.config().virtual_text }
+      end,
+    },
+    config = function(_, opts)
+      require('corn').setup(opts)
+    end,
+  },
 }
--- .config/nvim/lua/plugins/motions.lua
+++ .config/nvim/lua/plugins/motions.lua
@@ -1,68 +1,38 @@
 return {
-  -- {
-  --   "phaazon/hop.nvim",
-  --   lazy = true,
-  --   cmd = {
-  --     "HopWord",
-  --     "HopChar1",
-  --     "HopLine",
-  --   },
-  --   init = function()
-  --     vim.keymap.set("n", "<leader>ff", function()
-  --       vim.cmd([[:HopWord]])
-  --     end, { desc = "Hop To Word" })
-  --     vim.keymap.set("n", "<leader>fl", function()
-  --       vim.cmd([[:HopLine]])
-  --     end, { desc = "Hop To Line" })
-  --   end,
-  --   config = function()
-  --     require("hop").setup()
-  --   end,
-  -- },
-  -- {
-  --   "folke/flash.nvim",
-  --   event = "VeryLazy",
-  --   ---@type Flash.Config
-  --   opts = {
-  --     search = {
-  --       -- search/jump in all windows
-  --       multi_window = true,
-  --       -- search direction
-  --       forward = true,
-  --       -- when `false`, find only matches in the given direction
-  --       wrap = true,
-  --       ---@type Flash.Pattern.Mode
-  --       -- Each mode will take ignorecase and smartcase into account.
-  --       -- * exact: exact match
-  --       -- * search: regular search
-  --       -- * fuzzy: fuzzy search
-  --       -- * fun(str): custom function that returns a pattern
-  --       --   For example, to only match at the beginning of a word:
-  --       --   mode = function(str)
-  --       --     return "\\<" .. str
-  --       --   end,
-  --       mode = "exact",
-  --       -- behave like `incsearch`
-  --       incremental = false,
-  --       filetype_exclude = { "notify", "noice" },
-  --     },
-  --   },
-  --   keys = {
-  --     {
-  --       "<leader>f",
-  --       mode = { "n", "x", "o" },
-  --       function()
-  --         -- default options: exact mode, multi window, all directions, with a backdrop
-  --         require("flash").jump()
-  --       end,
-  --     },
-  --     {
-  --       "<leader>F",
-  --       mode = { "o", "x" },
-  --       function()
-  --         require("flash").treesitter()
-  --       end,
-  --     },
-  --   },
-  -- },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    'ThePrimeagen/harpoon',
+    opts = {},
+    config = function(_, opts)
+      require('harpoon').setup(opts)
+
+      vim.api.nvim_set_keymap('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<cr>', { desc = 'Harpoon add file' })
+      vim.api.nvim_set_keymap('n', '<leader>hj', '<cmd>lua require("harpoon.ui").nav_next()<cr>', { desc = 'Harpoon next' })
+      vim.api.nvim_set_keymap('n', '<leader>hk', '<cmd>lua require("harpoon.ui").nav_prev()<cr>', { desc = 'Harpoon prev' })
+      vim.api.nvim_set_keymap('n', '<leader>hh', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', { desc = 'Harpoon ui' })
+
+      vim.api.nvim_set_keymap('n', '<leader>h1', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', { desc = 'Harpoon to file 1' })
+      vim.api.nvim_set_keymap('n', '<leader>h2', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', { desc = 'Harpoon to file 2' })
+      vim.api.nvim_set_keymap('n', '<leader>h3', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', { desc = 'Harpoon to file 3' })
+      vim.api.nvim_set_keymap('n', '<leader>h4', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', { desc = 'Harpoon to file 4' })
+      vim.api.nvim_set_keymap('n', '<leader>h5', '<cmd>lua require("harpoon.ui").nav_file(5)<cr>', { desc = 'Harpoon to file 5' })
+      vim.api.nvim_set_keymap('n', '<leader>h6', '<cmd>lua require("harpoon.ui").nav_file(6)<cr>', { desc = 'Harpoon to file 6' })
+      vim.api.nvim_set_keymap('n', '<leader>h7', '<cmd>lua require("harpoon.ui").nav_file(7)<cr>', { desc = 'Harpoon to file 7' })
+      vim.api.nvim_set_keymap('n', '<leader>h8', '<cmd>lua require("harpoon.ui").nav_file(8)<cr>', { desc = 'Harpoon to file 8' })
+      vim.api.nvim_set_keymap('n', '<leader>h9', '<cmd>lua require("harpoon.ui").nav_file(9)<cr>', { desc = 'Harpoon to file 9' })
+    end,
+  },
 }
