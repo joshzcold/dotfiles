ac7d35d92259ba459c3aa2bef97ed21ed567e38a
Joshua Cold <joshua.cold@securitymetrics.com>
2024-02-05 10:02:48 -0700

    update dotfiles from 00666-joshua-cold

---
+++ .config/nvim/lua/plugins/python.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "AckslD/swenv.nvim",
+    config = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>pv",
+        "<cmd>lua require('swenv.api').pick_venv()<cr>",
+        { desc = "Python pick venv" }
+      )
+
+      require("swenv").setup({
+        -- Should return a list of tables with a `name` and a `path` entry each.
+        -- Gets the argument `venvs_path` set below.
+        -- By default just lists the entries in `venvs_path`.
+        get_venvs = function(venvs_path)
+          return require("swenv.api").get_venvs(venvs_path)
+        end,
+        -- Path passed to `get_venvs`.
+        venvs_path = vim.fn.expand("~/.virtualenvs"),
+        -- Something to do after setting an environment, for example call vim.cmd.LspRestart
+        post_set_venv = function()
+          vim.cmd([[:LspRestart]])
+        end,
+      })
+    end,
+  },
+}
