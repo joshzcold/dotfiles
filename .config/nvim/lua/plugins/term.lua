436e952e656cd64671cb3e46773cfda1bfce9e64
Joshua Cold <joshua.cold@securitymetrics.com>
2024-03-14 13:11:36 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -1,31 +1,41 @@
 return {
 	{
 		"akinsho/toggleterm.nvim",
-		-- tag = "*",
 		init = function()
-			vim.keymap.set("n", "<leader>tt", function()
-				vim.cmd([[:ToggleTerm size=10 direction=tab]])
-			end, { desc = "Term Tab" })
+			--[[
+			--	Set up lots of key bindings to support up to 9 terminals at once
+			--]]
+			for i = 9, 1, -1 do
+				local n = i
+				if i == 1 then
+					---@diagnostic disable-next-line: cast-local-type
+					i = ""
+				end
+				vim.keymap.set("n", "<leader>t" .. i .. "t", function()
+					vim.cmd(":" .. n .. "ToggleTerm size=10 direction=tab")
+				end, { desc = "Term Tab " .. i })
 
-			vim.keymap.set("n", "<leader>tl", function()
-			  local width = vim.fn.winwidth(0)
-			  local set_width = width / 3
-			  vim.cmd(":ToggleTerm size=" .. set_width .. " direction=vertical")
-			end, { desc = "Term Right" })
+				vim.keymap.set("n", "<leader>t" .. i .. "l", function()
+					local width = vim.fn.winwidth(0)
+					local set_width = width / 3
+					vim.cmd(":" .. n .. "ToggleTerm size=" .. set_width .. " direction=vertical")
+				end, { desc = "Term Right " .. i })
 
-			vim.keymap.set("n", "<leader>th", function()
-				local width = vim.fn.winwidth(0)
-				local set_width = width / 3
-				vim.cmd(":ToggleTerm size=" .. set_width .. " direction=vertical")
-			end, { desc = "Term Left" })
+				vim.keymap.set("n", "<leader>t" .. i .. "j", function()
+					vim.cmd(":" .. n .. "ToggleTerm size=10 direction=horizontal")
+				end, { desc = "Term Below " .. i })
 
-			vim.keymap.set("n", "<leader>tj", function()
-				vim.cmd([[:ToggleTerm size=10 direction=horizontal]])
-			end, { desc = "Term Below" })
+				vim.keymap.set("n", "<leader>t" .. i .. "k", function()
+					vim.cmd(":" .. n .. "ToggleTerm size=10 direction=float")
+				end, { desc = "Term Float " .. i })
+			end
+			vim.keymap.set("n", "<leader>tx", function()
+				vim.cmd([[:ToggleTermToggleAll]])
+			end, { desc = "Term Toggle All" })
 
-			vim.keymap.set("n", "<leader>tk", function()
-				vim.cmd([[:ToggleTerm direction=float]])
-			end, { desc = "Term Float" })
+			vim.keymap.set("n", "<leader>t<leader>", function()
+				vim.cmd([[:TermSelect]])
+			end, { desc = "Term Select" })
 
 			-- misc keymappings jira
 			vim.keymap.set("n", "<leader>jjn", function()
@@ -51,9 +61,7 @@ return {
 				start_in_insert = true,
 				auto_scroll = false,
 			})
-			vim.cmd([[
-                au TermOpen * setlocal nospell
-            ]])
+			vim.cmd([[ au TermOpen * setlocal nospell ]])
 		end,
 	},
 }
