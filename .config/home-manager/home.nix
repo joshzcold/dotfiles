cd43d94709ce103be69da51c7260bc9f76726c9f
Joshua Cold <joshua.cold@securitymetrics.com>
2024-05-04 14:18:10 -0600

    update dotfiles from joshua-pixel-book

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -64,6 +64,8 @@ in
     pkgs.fzf
     pkgs.ripgrep
     pkgs.cargo
+    pkgs.volctl
+    pkgs.unclutter
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
