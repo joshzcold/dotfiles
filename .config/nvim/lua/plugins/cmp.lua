8bac0ea9464ee6436b728b87ff54235d3236e3ac
Joshua Cold <joshua.cold@securitymetrics.com>
2024-06-04 14:38:27 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,42 +1,42 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     lazy = false,
-    event = 'InsertEnter',
+    event = "InsertEnter",
     dependencies = {
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-cmdline' },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "L3MON4D3/LuaSnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
       -- { 'FelipeLema/cmp-async-path' }
     },
     config = function()
       -- Setup nvim-cmp.
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
       local default_sources = {
-        { name = 'luasnip' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
         {
-          name = 'buffer',
+          name = "buffer",
           option = {
             get_bufnrs = function()
               return vim.api.nvim_list_bufs()
             end,
           },
         },
-        { name = 'nvim_lua' },
+        { name = "lazydev",   group_index = 0 },
         { name = "async_path" },
       }
 
-      cmp.setup {
+      cmp.setup({
         formatting = {
-          format = lspkind.cmp_format { with_text = false, maxwidth = 50 },
+          format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
         },
         window = {
           completion = cmp.config.window.bordered(),
@@ -44,41 +44,41 @@ return {
         },
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         mapping = {
-          ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-          ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-          ['<C-e>'] = cmp.mapping {
+          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-y>"] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
+          ["<C-e>"] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
-          },
-          ['<CR>'] = cmp.mapping.confirm {},
-          ['<Tab>'] = function(fallback)
+          }),
+          ["<CR>"] = cmp.mapping.confirm({}),
+          ["<Tab>"] = function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             else
               fallback()
             end
           end,
-          ['<S-Tab>'] = function(fallback)
+          ["<S-Tab>"] = function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             else
               fallback()
             end
           end,
-          ['<C-j>'] = function(fallback)
+          ["<C-j>"] = function(fallback)
             if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
           end,
-          ['<C-k>'] = function(fallback)
+          ["<C-k>"] = function(fallback)
             if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
@@ -87,24 +87,24 @@ return {
           end,
         },
         sources = cmp.config.sources(default_sources),
-      }
-      cmp.setup.cmdline(':', {
+      })
+      cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-          { name = 'path' },
+          { name = "path" },
         }, {
-          { name = 'cmdline' },
+          { name = "cmdline" },
         }),
       })
-      cmp.setup.cmdline({ '/', '?' }, {
+      cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
-          { name = 'buffer' },
+          { name = "buffer" },
         },
       })
 
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
     end,
   },
 }
