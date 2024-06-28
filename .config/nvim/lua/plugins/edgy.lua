83a6f1b990751fb4e304dbba44479abf7c4493cd
Joshua Cold <joshua.cold@securitymetrics.com>
2024-06-28 13:27:23 -0600

    update dotfiles from joshua-pixel-book

--- .config/nvim/lua/plugins/edgy.lua
+++ .config/nvim/lua/plugins/edgy.lua
@@ -1,90 +1,66 @@
 return {
---   "folke/edgy.nvim",
---   event = "VeryLazy",
---   opts = {
---     animate = {
---       enabled = false,
---       fps = 200, -- frames per second
---       cps = 240, -- cells per second
---       on_begin = function()
---         vim.g.minianimate_disable = true
---       end,
---       on_end = function()
---         vim.g.minianimate_disable = false
---       end,
---       -- Spinner for pinned views that are loading.
---       -- if you have noice.nvim installed, you can use any spinner from it, like:
---       -- spinner = require("noice.util.spinners").spinners.circleFull,
---       spinner = {
---         frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
---         interval = 80,
---       },
---     },
---     options = {
---       left = { size = 30 },
---       bottom = { size = 10 },
---       right = { size = 30 },
---       top = { size = 10 },
---     },
---     bottom = {
---       "Trouble",
---       { ft = "qf",       title = "QuickFix" },
---       { ft = "fugitive", title = "Git",     size = { height = 20 } },
---       {
---         ft = "help",
---         size = { height = 20 },
---         -- only show help buffers
---         filter = function(buf)
---           return vim.bo[buf].buftype == "help"
---         end,
---       },
---       { ft = "spectre_panel", size = { height = 0.4 } },
---     },
---     left = {
---       -- Neo-tree filesystem always takes half the screen height
---       {
---         title = "Neo-Tree",
---         ft = "neo-tree",
---         filter = function(buf)
---           return vim.b[buf].neo_tree_source == "filesystem"
---         end,
---         size = { height = 0.5, width = 0.3 },
---       },
---       {
---         title = "Neo-Tree Git",
---         ft = "neo-tree",
---         filter = function(buf)
---           return vim.b[buf].neo_tree_source == "git_status"
---         end,
---         pinned = true,
---         open = "Neotree position=right git_status",
---       },
---       {
---         title = "Neo-Tree Buffers",
---         ft = "neo-tree",
---         filter = function(buf)
---           return vim.b[buf].neo_tree_source == "buffers"
---         end,
---         pinned = true,
---         open = "Neotree position=top buffers",
---       },
---       {
---         ft = "Outline",
---         pinned = true,
---         open = "SymbolsOutline",
---       },
---       -- any other neo-tree windows
---       "neo-tree",
---     },
---     right = {
---       {
---         ft = "toggleterm",
---         size = { width = 0.35 },
---         -- exclude floating windows
---         filter = function(buf, win)
---           return vim.api.nvim_win_get_config(win).relative == ""
---         end,
---       },
---     },
---   },
+  "folke/edgy.nvim",
+  event = "VeryLazy",
+  opts = {
+    animate = {
+      enabled = false,
+      fps = 200, -- frames per second
+      cps = 240, -- cells per second
+      on_begin = function()
+        vim.g.minianimate_disable = true
+      end,
+      on_end = function()
+        vim.g.minianimate_disable = false
+      end,
+      -- Spinner for pinned views that are loading.
+      -- if you have noice.nvim installed, you can use any spinner from it, like:
+      -- spinner = require("noice.util.spinners").spinners.circleFull,
+      spinner = {
+        frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+        interval = 80,
+      },
+    },
+    options = {
+      left = { size = 30 },
+      bottom = { size = 10 },
+      right = { size = 30 },
+      top = { size = 10 },
+    },
+    bottom = {
+      "Trouble",
+      { ft = "qf",       title = "QuickFix" },
+      { ft = "fugitive", title = "Git",     size = { height = 20 } },
+      {
+        ft = "help",
+        size = { height = 20 },
+        -- only show help buffers
+        filter = function(buf)
+          return vim.bo[buf].buftype == "help"
+        end,
+      },
+    },
+    left = {
+      -- Neo-tree filesystem always takes half the screen height
+      {
+        title = "NvimTree",
+        ft = "NvimTree",
+        size = { height = 0.5, width = 0.3 },
+      },
+      {
+        ft = "Outline",
+        pinned = true,
+        open = "SymbolsOutline",
+      },
+    },
+    right = {
+      -- {
+      --   ft = "toggleterm",
+      --   size = { width = 0.35 },
+      --   -- exclude floating windows
+      --   filter = function(buf, win)
+      --     return vim.api.nvim_win_get_config(win).relative == ""
+      --   end,
+      -- },
+    },
+  },
 }
