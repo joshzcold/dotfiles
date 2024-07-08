546b7977d22b68798ad8f7ffa48336e93e613074
Joshua Cold <joshua.cold@securitymetrics.com>
2024-07-08 14:00:14 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -116,7 +116,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "commitmsg", "gitcommit" },
 	callback = function()
 		map("n", "<leader>a", "<cmd>AnsibleRequirementsBumpGitCommit<cr>", { desc = "Ansible insert new role bumps" })
-		require('gitsigns').detach()
 	end,
 })
 
