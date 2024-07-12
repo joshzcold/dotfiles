ad90fad283c5df9bcf45f55aae154f57219470d7
Joshua Cold <joshua.cold@securitymetrics.com>
2024-07-11 21:51:30 -0600

    update dotfiles from joshua-home

--- .config/install-setup.sh
+++ .config/install-setup.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-function user_prompt(){
+function user_prompt() {
   read -p "$1 [y/n]" -n 1 -r
-  echo    # (optional) move to a new line
+  echo # (optional) move to a new line
 }
 
 user_prompt "Install packages from apt"
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   sudo apt install -y \
     git \
@@ -19,27 +18,17 @@ then
     python3-pip \
     python3-virtualenv \
     python3-virtualenvwrapper
-  { set +x; } 2> /dev/null
-fi
-
-user_prompt "use yadm and get dotfiles"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  set -x
-  yadm clone https://github.com/joshzcold/dotfiles.git
-  yadm checkout .
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Setup dwm"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   cd ~/.dwm
   sudo apt install make libxft-dev libxcb-res0-dev libx11-dev libxinerama-dev libfontconfig-dev libx11-xcb-dev
   sudo make install
   sudo cp run-dwm /usr/bin/run-dwm
-  sudo tee /usr/share/xsessions/dwm.desktop > /dev/null <<EOT
+  sudo tee /usr/share/xsessions/dwm.desktop >/dev/null <<EOT
 [Desktop Entry]
 Version=1.0
 Name=dwm
@@ -47,108 +36,109 @@ Exec=run-dwm
 Icon=
 Type=Application
 EOT
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Setup packages"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   cd ~
   sudo apt install zsh curl
-  sh <(curl -L https://nixos.org/nix/install) --daemon
+  if ! command -v nix-channel; then
+    sh <(curl -L https://nixos.org/nix/install) --daemon
+  fi
+
+  if ! command -v nix-channel; then
+    echo "You now need to exit the shell and restart this script"
+    exit
+  fi
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
   nix-shell '<home-manager>' -A install
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Setup dictionary qutebrowser"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   /home/joshua/.nix-profile/share/qutebrowser/scripts/dictcli.py install en-US
-  { set +x; } 2> /dev/null 
-fi
-
-user_prompt "Setup ssh keys go to github.com and copy ssh keys to ~/.ssh"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  set -x
-  read -p "MANUAL STEP"
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Clone code-paste to ~/git"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   mkdir -p ~/git
   git clone git@github.com:joshzcold/codepaste.git
 fi
 
 user_prompt "Setup kmonad"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   sudo groupadd uinput
   sudo usermod -aG input $USER
   sudo usermod -aG uinput $USER
   echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/90-kmonad.rules
   sudo modprobe uinput
-  sudo ln -s /home/joshua/.nix-profile/bin/kmonad /usr/bin/kmonad
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "setup perimeter81"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   yay --save --editmenu -S perimeter81
   user_prompt "Add --no-sandbox to perimeter81.desktop Exec"
   read -p "MANUAL STEP"
   sudo nvim /usr/share/applications/perimeter81.desktop
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Install neovim plugins"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   nvim --headless "+Lazy! sync" +qa
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Change yadm remote to ssh"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   yadm remote set-url origin git@github.com:joshzcold/dotfiles.git
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Clone all securitymetrics repos"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   set -x
   bash ~/.config/usr-scripts/clone_all_bb.sh
-  { set +x; } 2> /dev/null 
+  { set +x; } 2>/dev/null
 fi
 
 user_prompt "Install nodejs dependencies for qutebrowser"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   # user specific packages
   cd ~/.config/qutebrowser/userscripts
   npm install qutejs
 fi
 
-user_prompt "Setup printing"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  sudo pacman -S cups avahi nss-mdns
-  echo "Put this in your /etc/nsswitch.conf"
-  echo "hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns"
-  user_prompt ""
-  sudo systemctl enable --now cups avahi-daemon
+user_prompt "Install bob neovim"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  cd /opt
+  sudo wget https://github.com/MordechaiHadad/bob/releases/download/v3.0.1/bob-linux-x86_64.zip
+  sudo unzip bob-linux-x86_64.zip
 fi
 
+user_prompt "Symlink apps into /usr/local/bin"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  cd /usr/local/bin
+  sudo ln -s "$(command -v kmonad)"
+  sudo ln -s /home/joshua/.local/share/bob/nvim-bin/nvim
+  sudo ln -s /opt/bob-linux-x86_64/bob
+fi
+
+user_prompt "Symlink ssh key from codepaste"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  cd ~/.ssh
+  rm id_rsa*
+  ln -s "$HOME/git/codepaste/id_rsa"
+  ln -s "$HOME/git/codepaste/id_rsa.pub"
+fi
