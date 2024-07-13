7ef93f16135ca1821289f954ce09ebe424d2b35e
Joshua Cold <joshua.cold@securitymetrics.com>
2024-07-13 16:08:10 -0600

    update dotfiles from joshua-pixel-book

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,23 +2,26 @@ return {
   "folke/which-key.nvim",
   config = function()
     require("which-key").setup({
+      modes = {
+        x = false,
+      },
       plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        marks = true,       -- shows a list of your marks on ' and `
+        registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
           suggestions = 20, -- how many suggestions should be shown in the list?
         },
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-          operators = true, -- adds help for operators like d, y, ... And registers them for motion / text object completion
-          motions = true, -- adds help for motions
+          operators = true,    -- adds help for operators like d, y, ... And registers them for motion / text object completion
+          motions = true,      -- adds help for motions
           text_objects = true, -- help for text objects triggered after entering an operator
-          windows = true, -- default bindings on <c-w>
-          nav = true, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
+          windows = true,      -- default bindings on <c-w>
+          nav = true,          -- misc bindings to work with windows
+          z = true,            -- bindings for folds, spelling and others prefixed with z
+          g = true,            -- bindings for prefixed with g
         },
       },
       -- add operators that will trigger motion and text object completion
@@ -38,25 +41,25 @@ return {
       },
       popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
       },
       window = {
-        border = "none", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "none",          -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
       },
       layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        height = { min = 4, max = 25 },                                             -- min and max height of the columns
+        width = { min = 20, max = 50 },                                             -- min and max width of the columns
+        spacing = 3,                                                                -- spacing between columns
+        align = "left",                                                             -- align columns left, center or right
       },
-      ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+      ignore_missing = false,                                                       -- enable this to hide mappings for which you didn't specify a label
       hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-      show_help = true, -- show help message on the command line when the popup is visible
-      triggers = "auto", -- automatically setup triggers
+      show_help = true,                                                             -- show help message on the command line when the popup is visible
+      triggers = "auto",                                                            -- automatically setup triggers
       -- triggers = {"<leader>"} -- or specify a list manually
       triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
@@ -69,26 +72,26 @@ return {
 
     local wk = require("which-key")
     wk.add({
-  {
-    { "<leader>/", group = "Find" },
-    { "<leader>b", group = "Buffers" },
-    { "<leader>d", group = "Debug" },
-    { "<leader>da", group = "Control" },
-    { "<leader>db", group = "Breakpoints" },
-    { "<leader>dh", group = "Hover" },
-    { "<leader>dr", group = "Repl" },
-    { "<leader>ds", group = "Step" },
-    { "<leader>du", group = "UI" },
-    { "<leader>f", group = "Jump" },
-    { "<leader>g", group = "Git" },
-    { "<leader>j", group = "Misc" },
-    { "<leader>jj", group = "Jira Actions" },
-    { "<leader>s", group = "Substitute" },
-    { "<leader>t", group = "Terminal" },
-    { "<leader>v", group = "Vim" },
-    { "<leader>vp", group = "Profile vim" },
-    { "<leader>y", group = "Yank" },
-  }
+      {
+        { "<leader>/",  group = "Find" },
+        { "<leader>b",  group = "Buffers" },
+        { "<leader>d",  group = "Debug" },
+        { "<leader>da", group = "Control" },
+        { "<leader>db", group = "Breakpoints" },
+        { "<leader>dh", group = "Hover" },
+        { "<leader>dr", group = "Repl" },
+        { "<leader>ds", group = "Step" },
+        { "<leader>du", group = "UI" },
+        { "<leader>f",  group = "Jump" },
+        { "<leader>g",  group = "Git" },
+        { "<leader>j",  group = "Misc" },
+        { "<leader>jj", group = "Jira Actions" },
+        { "<leader>s",  group = "Substitute" },
+        { "<leader>t",  group = "Terminal" },
+        { "<leader>v",  group = "Vim" },
+        { "<leader>vp", group = "Profile vim" },
+        { "<leader>y",  group = "Yank" },
+      },
     }, { prefix = "<leader>" })
   end,
 }
