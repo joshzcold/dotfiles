ef386e0a63ba3ff93604885382211c68892640a6
Joshua Cold <joshua.cold@securitymetrics.com>
2024-07-16 12:50:36 -0600

    update dotfiles from 00666-joshua-cold

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -75,6 +75,7 @@ in
     pkgs.ansible
     pkgs.ansible-lint
     pkgs.htop
+    pkgs.groovy
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
