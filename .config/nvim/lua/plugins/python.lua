d9ebaa9e51fa8544649fdc6b71b49c72dcd9fdec
Joshua Cold <joshua.cold@securitymetrics.com>
2024-07-18 13:02:50 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -44,7 +44,7 @@ return {
         venvs_path = vim.fn.expand("~/.virtualenvs"),
         -- Something to do after setting an environment, for example call vim.cmd.LspRestart
         post_set_venv = function()
-          vim.cmd([[:e]])
+          vim.cmd([[:LspRestart]])
         end,
       })
     end,
