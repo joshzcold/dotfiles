2006f684eb94eb03236a89e723573bae343a7f6a
Joshua Cold <joshua.cold@securitymetrics.com>
2024-08-06 16:51:48 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -45,15 +45,13 @@ return {
             function()
               return require("lsp-status").status()
             end,
-            function()
-              if vim.bo.filetype == "python" then
-                local current_path = require("swenv.api").get_current_venv().path
-                local venv_name = vim.fs.basename(vim.fs.dirname(current_path)) .. "/" .. vim.fs.basename(current_path)
-                return "venv: " .. venv_name
-              else
-                return ""
-              end
-            end,
+            {
+              "swenv",
+              icon = "",
+              cond = function()
+                return vim.bo.filetype == "python"
+              end,
+            },
             {
               require("noice").api.status.mode.get,
               cond = require("noice").api.status.mode.has,
