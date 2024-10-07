26f2103ade2c03dd82a6ad0de7a559ead4384538
Joshua Cold <joshua.cold@securitymetrics.com>
2024-10-07 09:26:34 -0600

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -1,3 +1,24 @@
+local function get_jira_tag()
+  local Job = require("plenary.job")
+  local out_branch_tag = ""
+  Job:new({
+    command = "git",
+    args = { "rev-parse", "--abbrev-ref", "HEAD" },
+    on_exit = function(j, _)
+      local patterns = {
+        "(%a%w%d%-%d+)",
+        "(ENHANCEMENT)",
+        "(CHORE)",
+      }
+      for _, p in ipairs(patterns) do
+        local _, _, branch_tag = string.find(j:result()[1], p)
+        out_branch_tag = branch_tag
+      end
+    end,
+  }):sync()
+  return out_branch_tag
+end
+
 local function git_write_with_input()
   vim.cmd.Gwrite()
   vim.api.nvim_command(":!git add $(readlink -f %)")
@@ -65,7 +86,7 @@ vim.api.nvim_create_user_command("AnsibleLintFix", function()
   Job:new({
     command = "ansible-lint",
     args = { "--fix", "-L", "-x", "role-name", "--nocolor" },
-    on_exit = function(j, return_val)
+    on_exit = function(j, _)
       vim.schedule(function()
         if j.code ~= 0 then
           vim.notify(vim.inspect(j._stderr_results), vim.log.levels.ERROR)
@@ -81,7 +102,7 @@ vim.api.nvim_create_user_command("AnsibleLintFix", function()
               finder = require("telescope.finders").new_table({
                 results = result_list,
               }),
-              attach_mappings = function(prompt_bufnr, map)
+              attach_mappings = function(prompt_bufnr, _)
                 actions.select_default:replace(function()
                   actions.close(prompt_bufnr)
                   local selection = action_state.get_selected_entry()
@@ -94,9 +115,9 @@ vim.api.nvim_create_user_command("AnsibleLintFix", function()
                       local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
                       return table.concat(content, "\n")
                     end,
-                    on_exit = function(j, return_val)
+                    on_exit = function(k, _)
                       vim.schedule(function()
-                        if j.code ~= 0 then
+                        if k.code ~= 0 then
                           vim.notify(vim.inspect(j._stderr_results))
                         end
                       end)
@@ -118,7 +139,7 @@ vim.api.nvim_create_user_command("AnsibleRequirementsBumpGitCommit", function()
   Job:new({
     command = "git",
     args = { "diff", "--cached" },
-    on_exit = function(j, return_val)
+    on_exit = function(j, _)
       local last_version = nil
       local result_list = {}
       for _, v in pairs(j:result()) do
@@ -146,31 +167,14 @@ vim.api.nvim_create_user_command("AnsibleRequirementsBumpGitCommit", function()
 end, {})
 
 vim.api.nvim_create_user_command("InsertJiraTag", function()
-  local Job = require("plenary.job")
-  Job:new({
-    command = "git",
-    args = { "rev-parse", "--abbrev-ref", "HEAD" },
-    on_exit = function(j, return_val)
-      local patterns = {
-        "(%a%w%d%-%d+)",
-        "(ENHANCEMENT)",
-        "(CHORE)",
-      }
-      vim.schedule(function()
-        local buf_text = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-        for _, p in ipairs(patterns) do
-          local _, _, branch_tag = string.find(j:result()[1], p)
-          local _, _, already_in = string.find(buf_text[1], p)
-
-          if branch_tag and not already_in then
-            branch_tag = branch_tag .. " "
-            local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-            vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { branch_tag })
-            vim.api.nvim_win_set_cursor(0, { 1, branch_tag:len() + 2 })
-            vim.cmd([[:call feedkeys('A', 'n')]])
-          end
-        end
-      end)
-    end,
-  }):sync()
+  local branch_tag = get_jira_tag()
+  local buf_text = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local _, _, already_in = string.find(buf_text[1], branch_tag)
+  if branch_tag and not already_in then
+    branch_tag = branch_tag .. " "
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { branch_tag })
+    vim.api.nvim_win_set_cursor(0, { 1, branch_tag:len() + 2 })
+    vim.cmd([[:call feedkeys('A', 'n')]])
+  end
 end, {})
