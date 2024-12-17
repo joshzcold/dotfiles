e598c4fd69049853e958ef449dfb5dbe3d46f8e1
Joshua Cold <joshua.cold@securitymetrics.com>
2024-12-17 15:25:57 -0700

    update dotfiles from 00666-joshua-cold

--- .config/nvim/lua/plugins/text-obj.lua
+++ .config/nvim/lua/plugins/text-obj.lua
@@ -3,12 +3,14 @@ return {
         "chrisgrieser/nvim-various-textobjs",
         config = function()
             require("various-textobjs").setup({
-                useDefaultKeymaps = true ,
-                disabledKeymaps = {
-                    "L",
-                    "gc",
-                    "gb"
-                }
+                keymaps = {
+                    useDefault = true,
+                    disabled = {
+                        "L",
+                        "gc",
+                        "gb",
+                    },
+                },
             })
         end,
     },
