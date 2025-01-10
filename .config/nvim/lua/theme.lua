f01068805a0725afc34b379e527107b17decbfc8
Joshua Cold <joshua.cold@securitymetrics.com>
2025-01-10 15:41:29 -0700

    update dotfiles from 01816-joshua-cold

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -17,3 +17,9 @@ vim.cmd([[
 ]])
 
 vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "#0a0c0f", fg = "#6e7681", })
+
+
+local palette = require('github-theme.palette').load('github_dark_default')
+
+vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = palette.black.base, fg = palette.white.bright, })
+vim.api.nvim_set_hl(0, "TelescopeMatching", { bg = palette.black.base, fg = palette.red.bright, })
