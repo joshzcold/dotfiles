3aaeb439f01d9bd3a7cc80c06d8ecf7578dec5e8
Joshua Cold <joshua.cold@securitymetrics.com>
2025-03-06 11:16:05 -0700

    update dotfiles from 01816-joshua-cold

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -26,8 +26,8 @@ return {
         keymap = {
           ["<Tab>"] = { "select_next", "fallback" },
           ["<S-Tab>"] = { "select_prev", "fallback" },
-          ["<CR>"] = { "select_and_accept", "fallback" },
-          ['<C-e>'] = { 'cancel' },
+          ["<C-CR>"] = { "accept", "fallback" },
+          ['<C-c>'] = { 'cancel' },
         }
       },
       completion = {
