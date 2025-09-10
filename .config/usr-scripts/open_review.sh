975eb0523b74456e958bb423acd479f745e1907f
Joshua Cold <joshua.cold@securitymetrics.com>
2025-09-10 11:44:52 -0600

    update dotfiles from 01816-joshua-cold

--- .config/usr-scripts/open_review.sh
+++ .config/usr-scripts/open_review.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 set -eou pipefail
-project_name="$(git config --get remote.origin.url | rev | cut -d/ -f2 | rev)"
-repo_name="$(basename -s .git $(git config --get remote.origin.url))"
-branch_name="$(git rev-parse --abbrev-ref HEAD)"
+remote="$(git config --get remote.origin.url)"
 
-url="https://bitbucket.secmet.co/projects/${project_name}/repos/${repo_name}/pull-requests?create&sourceBranch=refs/heads/${branch_name}"
+if [[ "$remote" =~ .*bitbucket.* ]]; then
+	project_name="$(echo "$remote" | rev | cut -d/ -f2 | rev)"
+	repo_name="$(basename -s .git "$remote")"
+	branch_name="$(git rev-parse --abbrev-ref HEAD)"
+
+	url="https://bitbucket.secmet.co/projects/${project_name}/repos/${repo_name}/pull-requests?create&sourceBranch=refs/heads/${branch_name}"
+elif [[ "$remote" =~ .*github.* ]]; then
+	repo_name="$(basename -s .git "$remote")"
+	branch_name="$(git rev-parse --abbrev-ref HEAD)"
+	project_name="$(echo "$remote" | rev | cut -d: -f1 | cut -d/ -f2 |  rev)"
+	url="https://github.com/${project_name}/${repo_name}/compare/${branch_name}?expand=1"
+else
+	notify-send "$0: Unknown remote for this script: ${remote}"
+fi
+
 qutebrowser ":open -t $url"
