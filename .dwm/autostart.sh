a2cea4fec3883e3635515c5fa8cf44283461cf31
Joshua Cold <joshua.cold@securitymetrics.com>
2025-09-23 15:51:05 -0600

    update dotfiles from 01816-joshua-cold

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -10,7 +10,7 @@ picom &
 # keynav &
 blueman-applet &
 volctl &
-pkill -f check-on-dotfiles && "$HOME"/.config/usr-scripts/check-on-dotfiles &
-pkill -f check_bb_reviews.sh && "$HOME"/.config/usr-scripts/check_bb_reviews.sh &
-pkill -f check-on-todos.sh && "$HOME"/.config/usr-scripts/check-on-todos.sh &
+pkill -f check-on-dotfiles; "$HOME"/.config/usr-scripts/check-on-dotfiles &
+pkill -f check_bb_reviews.sh; "$HOME"/.config/usr-scripts/check_bb_reviews.sh &
+pkill -f check-on-todos.sh; "$HOME"/.config/usr-scripts/check-on-todos.sh &
 kdeconnect-indicator &
