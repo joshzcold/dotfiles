cd2702522313bdec93154393b0b26054e86b4a00
Joshua Cold <joshua.cold@securitymetrics.com>
2026-03-12 08:38:44 -0600

    update dotfiles from joshua-home

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -85,6 +85,7 @@ in
     pkgs.xfce.thunar
     pkgs.yadm
     pkgsUnstable.gh
+    pkgs.delta
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
