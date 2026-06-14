d9900ed9dc63bbac38c9ebed09375cf988b96fd3
Joshua Cold <joshua.cold@securitymetrics.com>
2026-06-14 16:44:21 -0600

    update dotfiles from 01816-joshua-cold

--- .config/usr-scripts/git_push.sh
+++ .config/usr-scripts/git_push.sh
@@ -5,5 +5,9 @@ push_out=$(git push -u 2>&1 | tee >(cat - >&5))
 
 if echo "$push_out" | grep "Create.*pull request"; then
 	url=$(echo "$push_out" | grep -oP "https://.*" | head -n 1)
-	qutebrowser ":open -t $url"
+	if [[ -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
+		printf "\033]52;c;%s\a" "$(printf '%s' "$url" | base64 -w0)"
+	else
+		qutebrowser ":open -t $url"
+	fi
 fi
