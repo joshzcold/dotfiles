c8a42d87af2b53743b9e07db027658a512bbf7bf
Joshua Cold <joshua.cold@securitymetrics.com>
2026-07-23 07:21:48 -0600

    update dotfiles from joshua-home

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,6 +3,7 @@
 export PATH="$HOME/.nix-profile/bin:$PATH"
 export XDG_DATA_DIRS="$HOME/.nix-profile/share:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
 
+xsettingsd &
 autorandr -c default
 nitrogen --restore
 # emacs --daemon &
